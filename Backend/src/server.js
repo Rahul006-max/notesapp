5e1354cb174e43c0a305d17123ed7cdd39c5b75a
Rahul <rahulp22106@gmail.com>
2025-08-26 14:44:14 +0530

    did all the required changes for it to run on render

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,18 +2,22 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
-import rateLimiter from "../middleware/ratelLimiter.js";
+import rateLimiter from "./middleware/ratelLimiter.js";
 import cors from "cors";
+import path from "path";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-); //url request
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  ); //url request
+}
 
 app.use(express.json());
 app.use(rateLimiter);
@@ -25,6 +29,12 @@ app.use((req, res, next) => {
 
 app.use("/api/notes", notesRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile.join(__dirname, "../frontend", "dist", "index.html");
+  });
+}
 connectDB().then(() => {
   app.listen(5001, () => {
     console.log("server is started on port:", PORT);
---
+++ Frontend/src/lib/axios.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const BaseURL =
+  import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+
+const api = axios.create({
+  baseURL: BaseURL,
+});
+
+export default api;
